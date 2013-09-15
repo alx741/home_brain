56b6f869daa8c6c2338bee0fd07ce3eea71e19c8
Geovanny Campoverde <geocampo_14@hotmail.com>
2013-09-15 00:40:39 -0500

    adding main buttons to app

--- app/src/com/example/homebrain1/MainActivity.java
+++ app/src/com/example/homebrain1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.homebrain1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void Start(View view) {
+        Intent i = new Intent(this, Control.class );
+        startActivity(i);
     }
     
 }
